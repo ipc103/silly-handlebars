656ab158fcc281001edbd99fccd9c0ea7801cb95
ipc103 <iancandysemail@gmail.com>
2015-11-18 16:47:31 -0500

    stupid project

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function(){
+  loadGifs();
+})
+
+function loadGifs() {
+  var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+  return $.getJSON(url, function(giphy){
+    var source   = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var rendered = template(giphy);
+    $('body').append(rendered);
+
+  })
+}
